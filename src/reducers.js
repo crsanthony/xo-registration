3f23b1d0025e10bf261fb1365e1a80b26f9d7fc8
Christopher Regan <crsanthony@gmail.com>
2017-05-23 16:32:25 -0400

    create reducer functions

---
+++ src/reducers.js
@@ -0,0 +1,18 @@
+import { TOGGLE_MODAL, SUBMIT_FORM } from '../constants/ActionTypes';
+
+const initialState {
+  modalOpen: false,
+  registered: false,
+}
+
+export default function registration(state = initialsState, action) {
+  switch(action.type) {
+    case TOGGLE_MODAL:
+     return (
+       ...state
+       modalOpen: !state.modalOpen
+     )
+    case SUBMIT_FORM:
+      return state
+  }
+}
