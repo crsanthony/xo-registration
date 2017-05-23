9099e9b690fff95f57e98dbf1cbca1131b48a8bd
Christopher Regan <crsanthony@gmail.com>
2017-05-23 11:20:29 -0400

    boilerplate and basic front end

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import Modal from 'react-modal';
+import logo from './assets/logo.png';
+import './App.css';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} />
+          <h2>Ready to take our relationship to the next level?</h2>
+        </div>
+        <p className="App-intro">
+          <button>Register</button>
+        </p>
+        <Modal isOpen={true}>
+          <span>hi there</span>
+        </Modal>
+      </div>
+    );
+  }
+}
+
+export default App;
