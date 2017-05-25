895b792a5777d4e9d4efb17d4d6e6ff47de8dd6d
Christopher Regan <crsanthony@gmail.com>
2017-05-25 15:41:48 -0400

    add some form field validation

--- src/components/RegistrationForm/RegistrationForm.js
+++ src/components/RegistrationForm/RegistrationForm.js
@@ -2,23 +2,33 @@ import React from 'react';
 import {Field, reduxForm} from 'redux-form';
 import './RegistrationForm.css';
 
+const required = value => value ? undefined : '';
+const password = value => value && value.length > 8;
+const email = value =>
+  value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value) ? 'hmm...that email is invalid' : undefined
+
+const renderField = ({ input, type, placeholder, meta: { touched, error, warning } }) => (
+  <div>
+    <input {...input} placeholder={placeholder} type={type} className="formField" />
+    {touched && ((error && <span>{error}</span>) || (warning && <span>{warning}</span>))}
+  </div>
+)
+
 const RegistrationForm = props => {
   let { submitHandler } = props;
   return (
     <form onSubmit={submitHandler} className="form">
       <div className="fields">
         <Field
-          className="formField"
           name="email"
           placeholder="email"
-          component="input"
-          type="text" />
+          type="text"
+          component={renderField} />
         <Field
-          className="formField"
-          name="email"
+          name="password"
           placeholder="password"
-          component="input"
-          type="password" />
+          type="password"
+          component={renderField} />
       </div>
       <button className="submitBtn" type="submit">Submit</button>
     </form>
@@ -26,5 +36,5 @@ const RegistrationForm = props => {
 }
 
 export default reduxForm({
-  form: 'regsistration' // a unique identifier for this form
+  form: 'regsistration'
 })(RegistrationForm)
