f1bb76bbf95d3c1aaefe0437cdf59d521a5bc898
Christopher Regan <crsanthony@gmail.com>
2017-05-25 21:11:28 -0400

    style error messaging

--- src/components/RegistrationForm/RegistrationForm.js
+++ src/components/RegistrationForm/RegistrationForm.js
@@ -3,14 +3,17 @@ import {Field, reduxForm} from 'redux-form';
 import './RegistrationForm.css';
 
 const required = value => value ? undefined : '';
-const password = value => value && value.length > 8;
+const password = value => {
+  console.log(value.length);
+  value && value.length > 8;
+}
 const email = value =>
   value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value) ? 'hmm...that email is invalid' : undefined
 
 const renderField = ({ input, type, placeholder, meta: { touched, error, warning } }) => (
-  <div>
+  <div className="formFieldContainer">
     <input {...input} placeholder={placeholder} type={type} className="formField" />
-    {touched && ((error && <span>{error}</span>) || (warning && <span>{warning}</span>))}
+    {touched && ((error && <span className="errorMsg">{error}</span>))}
   </div>
 )
 
@@ -18,21 +21,25 @@ const RegistrationForm = props => {
   let { submitHandler } = props;
   return (
     <form onSubmit={submitHandler} className="form">
+      <div className="fields">
         <Field
           name="email"
           placeholder="email"
+          validate={[required, email]}
           type="text"
           component={renderField} />
         <Field
           name="password"
           placeholder="password"
           type="password"
+          validate={required}
           component={renderField} />
+      </div>
       <button className="submitBtn" type="submit">Submit</button>
     </form>
   )
 }
 
 export default reduxForm({
-  form: 'regsistration'
+  form: 'registration'
 })(RegistrationForm)
